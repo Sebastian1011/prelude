3e9d3fee9db1b38b13d78f494177675f852a3403
ziming.zhang <ziming.zhang@yitu-inc.com>
2019-12-16 15:28:34 +0800

    upadte alert

--- bowmore/bowmore-alert.el
+++ bowmore/bowmore-alert.el
@@ -1,4 +1,7 @@
 (require 'org-alert)
 (setq alert-default-style 'osx-notifier)
+(setq org-alert-interval 300)
+(setq alert-fade-time 20)
+(setq alert-reveal-idle-time 200)
 
 (provide 'bowmore-alert)
